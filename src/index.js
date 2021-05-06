9c85a73edf0610507a56143207b0be533c055c25
Oksana Zelenevskaya <exoticisl@gmail.com>
2021-05-06 21:37:42 +0300

    Добавляет решение

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-import './sass/main.scss';
+// import './sass/main.scss';
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const refs = {
+    btnStart: document.querySelector('button[data-action="start"]'),
+    btnStop: document.querySelector('button[data-action="stop"]'),
+}
+
+let index = null;
+
+const randomIntegerFromInterval = (max) => {
+    index = Math.floor(Math.random() * max);
+    return index;
+};
+
+randomIntegerFromInterval(colors.length);
+console.log(index);
